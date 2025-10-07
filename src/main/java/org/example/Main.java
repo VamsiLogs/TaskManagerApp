1fc83b6645e8a5616d9ee2dfe0682296f54e5c99
VamsiKavety <vamsieverywhere@gmail.com>
2025-10-07 13:27:03 +0200

    Initial Release

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        System.out.println("Welcome to the Task Manager!");
+        System.out.println("----------------------------");
+
+        
+    }
+}
